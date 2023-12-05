9aa03bab7f3a028caeaaa9f5af583293bc22becd
Alexander Kevin <alexander.kevin@shortlyst.ai>
2023-12-05 21:34:37 +0700

    Add field id on createSuccessResponse
    - adjustment for email notification

--- src/utils/createSuccessResponse.ts
+++ src/utils/createSuccessResponse.ts
@@ -1,6 +1,7 @@
-export const createSuccessResponse = () => {
+export const createSuccessResponse = (id: number = 0) => {
   return {
     status: 200,
     message: 'Data Berhasil di Tambahkan',
+    id: id,
   };
 };
